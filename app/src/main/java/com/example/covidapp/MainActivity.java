8f83b3ec002d28c3e7cfbbfac8a8d0fbdf065e5c
GermanVq <gavegaq@gmail.com>
2020-11-24 11:41:28 -0500

    SplashScreen Added

--- app/src/main/java/com/example/covidapp/MainActivity.java
+++ app/src/main/java/com/example/covidapp/MainActivity.java
@@ -2,13 +2,42 @@ package com.example.covidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.drawable.Animatable;
+import android.media.Image;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+
+        // Agregar animaciones
+        Animation animacion1 = AnimationUtils.loadAnimation(this, R.anim.desplazamiento_arriba);
+        Animation animacion2 = AnimationUtils.loadAnimation(this, R.anim.desplazamiento_abajo);
+
+        TextView covTextView = findViewById(R.id.covidTextview);
+        ImageView logoImageView = findViewById(R.id.logoImage);
+
+        covTextView.setAnimation(animacion1);
+        logoImageView.setAnimation(animacion2);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2500);
     }
 }
